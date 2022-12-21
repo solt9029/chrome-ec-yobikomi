7d9f0d84cae3b09485efc23689a88d52216a3e09
Kenshi Shiode <atemu1212@yahoo.co.jp>
2022-12-21 09:07:57 +0900

    Remove console log

--- src/background.ts
+++ src/background.ts
@@ -17,18 +17,14 @@ audio.load();
 function polling() {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, tabs => {
         const url = tabs[0]?.url;
-        const isTargetUrl = url && targetUrls.map(targetUrl => url.indexOf(targetUrl)).some(index => index >= 0);
+        const isTargetUrl = url && targetUrls.some(targetUrl => url.indexOf(targetUrl) >= 0);
 
         if (!isPlayingMusic && isTargetUrl) {
-            console.log("this url is a target");
-
             isPlayingMusic = true;
             audio.play();
         }
 
         if (isPlayingMusic && !isTargetUrl) {
-            console.log("this url is not a target");
-
             isPlayingMusic = false;
             audio.pause();
             audio.currentTime = 0;
